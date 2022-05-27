441fc49961a39e95c31bfc781fbb22de430e9439
RbertKo <myeongsku@gmail.com>
2022-05-27 21:42:08 +0900

    :sparkles: LinkedList 구현

--- src/main/kotlin/com/tekovelop/DataStructure.kt
+++ src/main/kotlin/com/tekovelop/DataStructure.kt
@@ -1,5 +1,29 @@
 package com.tekovelop
 
+import com.tekovelop.data.structure.LinkedListImpl
+
 fun main(args: Array<String>) {
-    println("hi")
+    val list = LinkedListImpl<Long>()
+
+    list.plus(1)
+    list.plus(2)
+    list.plus(4)
+    list.plus(5)
+
+    println(list)
+
+    list.insertAt(2, 3)
+    list.insertAt(2, 3)
+
+    println(list)
+
+    list.deleteAt(2)
+
+    println(list)
+
+    val found = list.firstOrNull {
+        it == 4L
+    }
+
+    println(found)
 }
--- src/main/kotlin/com/tekovelop/data/structure/LinkedList.kt
+++ src/main/kotlin/com/tekovelop/data/structure/LinkedList.kt
@@ -1,4 +1,128 @@
 package com.tekovelop.data.structure
 
-class LinkedList {
+data class Node<T>(
+    val value: T,
+    var link: Node<T>? = null,
+)
+
+interface LinkedList<T> {
+    operator fun plus(item: T)
+
+    fun insertAt(index: Long, item: T)
+
+    fun deleteAt(index: Long)
+
+    fun firstOrNull(predicate: (T) -> Boolean): Node<T>?
+
+    fun firstOrNull(value: T): Node<T>?
+
+    fun getOrNull(index: Long): Node<T>?
+}
+
+class LinkedListImpl<T> : LinkedList<T> {
+    private var head: Node<T>? = null
+    private var count: Long = 0
+
+    override operator fun plus(item: T) {
+        if(head == null) {
+            head = Node(item)
+            count++
+            return
+        }
+
+        var node = head
+
+        while(node?.link != null) {
+            node = node.link
+        }
+
+        node?.link = Node(item)
+        count++
+    }
+
+    override fun insertAt(index: Long, item: T) {
+        var cur = 0L
+
+        if(cur == index && head == null) {
+            head = Node(item)
+            count++
+            return
+        }
+
+        var node = head
+
+        while(cur++ != index) {
+            if (node == null) throw NullPointerException("값을 찾을 수 없습니다.")
+
+            if(cur == index) {
+                val temp = node.link
+                val newNode = Node(item, temp)
+                node.link = newNode
+                count++
+                return
+            }
+
+            node = node.link
+        }
+    }
+
+    override fun deleteAt(index: Long) {
+        var cur = 0L
+        var node = head
+
+        if(cur++ == index && head != null) {
+            head = head?.link
+            count--
+            return
+        }
+
+        do {
+            if (node == null) throw NullPointerException("값을 찾을 수 없습니다.")
+
+            if(cur == index) {
+                node.link = node.link?.link
+                count--
+                return
+            }
+
+            node = node.link
+        } while(cur++ != index)
+    }
+
+    override fun firstOrNull(predicate: (T) -> Boolean): Node<T>? {
+        var node = head
+
+        while (node != null) {
+            if (predicate(node.value)) return node
+
+            node = node.link
+        }
+
+        return null
+    }
+
+    override fun firstOrNull(value: T): Node<T>? {
+        var node = head
+
+        while (node != null) {
+            if (node.value == value) return node
+
+            node = node.link
+        }
+
+        return null
+    }
+
+    override fun getOrNull(index: Long): Node<T>? {
+        var cur = 0L
+        var node = head
+
+        while(cur++ != index) {
+            if(cur == index || node == null) return node
+
+            node = node.link
+        }
+
+        return null
+    }
 }
